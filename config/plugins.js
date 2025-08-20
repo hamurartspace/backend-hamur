464bda33b4c737c3761c59d4548c2d02b783716c
hamurartspace <hamurartspace@gmail.com>
2025-08-20 14:45:30 +0700

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,24 +1,16 @@
-module.exports = ({ env }) => {
-  console.log("plugins.js loaded");
-  console.log("Cloudinary config:", {
-    cloud_name: env("CLOUDINARY_NAME"),
-    api_key: env("CLOUDINARY_KEY"),
-    api_secret: env("CLOUDINARY_SECRET"),
-  });
-  return {
-    upload: {
-      config: {
-        provider: "@strapi/provider-upload-cloudinary",
-        providerOptions: {
-          cloud_name: env("CLOUDINARY_NAME"),
-          api_key: env("CLOUDINARY_KEY"),
-          api_secret: env("CLOUDINARY_SECRET"),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: '@strapi/provider-upload-cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-  };
-};
+  },
+});
