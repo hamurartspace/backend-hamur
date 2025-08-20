b1f23a8ef58e8c96d0f10f36f846374df9f20512
hamurartspace <hamurartspace@gmail.com>
2025-08-20 14:33:09 +0700

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,10 @@
 module.exports = ({ env }) => {
   console.log("plugins.js loaded");
+  console.log("Cloudinary config:", {
+    cloud_name: env("CLOUDINARY_NAME"),
+    api_key: env("CLOUDINARY_KEY"),
+    api_secret: env("CLOUDINARY_SECRET"),
+  });
   return {
     upload: {
       config: {
